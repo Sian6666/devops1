95cc27d24a4e3b9e7ac3b74f23e26232047f7681
Sian6666 <bhuvanreddy5715@gmail.com>
2026-03-10 15:34:27 +0530

    Add source code

--- src/test/java/pgrm/resulttest.java
+++ src/test/java/pgrm/resulttest.java
@@ -1,19 +1,20 @@
-
-
 package pgrm;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class resulttest {
-@Test
-public void testeven() {
-	Assert.assertEquals(result.display(20),"even");
-	
+public class resultTest {
+	@Test
+	public void testeven() {
+		Assert.assertEquals(result.display(20),"even");
+		
+	}
+	@Test
+	public void testodd() {
+		Assert.assertEquals(result.display(21),"odd");
+		
+		
+	}
+
 }
-@Test
-public void testodd() {
-	Assert.assertEquals(result.display(21),"odd");
-	
-	
-}
-}
+
+
